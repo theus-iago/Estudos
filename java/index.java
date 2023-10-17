5aad081c121c4de3da5350eec74515f0be71a2be
theus-iago <ms2283903@gmail.com>
2023-10-17 06:35:51 -0300

    update03

--- java/index.java
+++ java/index.java
@@ -10,35 +10,51 @@ public class Index {
     int op = 0;
     int cont = 0;
     do {
-      System.out.println("\n\tBanco\n");
-      System.out.println("Menu\n");
-      System.out.println("1-Cadastrar\n");
-      System.out.println("2-Visualizar\n");
-      System.out.println("3-Sair\n");
-      System.out.println("Digite a sua opcao:");
+      System.out.println("|===================================");
+      System.out.println("|\tBanco");
+      System.out.println("|===================================");
+      System.out.println("|  Menu");
+      System.out.println("|===================================");
+      System.out.println("|1-Cadastrar");
+      System.out.println("|===================================");
+      System.out.println("|2-Visualizar");
+      System.out.println("|===================================");
+      System.out.println("|3-Sair");
+      System.out.println("|===================================");
+      System.out.print("|Digite a sua opcao:");
       op = sc.nextInt();
+      System.out.println("|===================================");
       switch(op) {
         case 1:
-          System.out.println("\n\tCadastro\n");
-          System.out.println("Digite o seu nome:");
+          System.out.println("|\tCadastro");
+      System.out.println("|===================================");
+          System.out.print("|Digite o seu nome:");
           nome = sc.next();
-          System.out.println("Digite a sua idade:");
+      System.out.println("|===================================");
+          System.out.print("|Digite a sua idade:");
           idade = sc.nextInt();
+      System.out.println("|===================================");
           lista.add(nome);
           break;
         case 2:
           break;
         default:
-        System.out.println("Esta opcao nao existe!");
+        System.out.println("|Esta opcao nao existe!");
+      System.out.println("|===================================");
           break;
         case 3:
-          System.out.println("Deseja mesmo sair?");
-          System.out.println("1-Sim\n");
-          System.out.println("2-Nao\n");
-          System.out.println("Digite a opcao:");
+          System.out.println("|Deseja mesmo sair?");
+      System.out.println("|===================================");
+          System.out.println("|1-Sim");
+      System.out.println("|===================================");
+          System.out.println("|2-Nao");
+      System.out.println("|===================================");
+          System.out.print("|Digite a opcao:");
           op = sc.nextInt();
+      System.out.println("|===================================");
           if(op == 1) {
-            System.out.println("Good Bye!");
+            System.out.println("|Good Bye!");
+      System.out.println("|===================================");
             op = 3;
           }else {
            break;
