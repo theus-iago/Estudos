fa47de31ca0a4b18cafb1959d912899e4de08b2c
cinematec <cinetec33@gmail.com>
2023-11-05 20:10:00 -0300

    up

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,23 @@
+/*IIFE functions*/
 (function() {
-  console.log("Teste");
+  console.log("Esta é uma IIFE!");
 })();
+
+//--------------×----------------//
+
+/*Callback functions*/
+function somar(n1,n2,callback) {
+  const resultado = n1 + n2;
+  callback(resultado);
+}
+function soma(soma) {
+  console.log("O resultado é:",soma);
+}
+somar(5,5,soma);
+
+//--------------×----------------//
+
+/*Arrow functions*/
+const arrow = () => {
+  console.log("arrow function!");
+}
